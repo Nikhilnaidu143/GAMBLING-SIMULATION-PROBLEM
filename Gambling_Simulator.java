f69177da7fea07fc16d8696e5c71d13ef5c8d076
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-20 10:13:58 +0530

    UC-5 :- Gambled for month and printed number of wins and looses.

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -3,11 +3,14 @@ package com.gambling.simulator;
 public class Gambling_Simulator {
 	/** Instance variables **/
 	static int Account_balance = 100;   // player start betting daily with a stake of 100.
-	static int num_of_wins_in_day = 0;
-	static int num_of_losses_in_day = 0;
+	static int num_of_wins_in_day = 0;  // This for each day.
+	static int num_of_losses_in_day = 0; // This for each day.
+	
+	static int num_of_wins_in_month = 0;  // This for whole month. 
+	static int num_of_losses_in_month = 0;  // This for whole month.
 	
 	//constant variable
-	static final int BET = 1;  // player bets 1 for every game
+	static final int BET = 1;  // player bets 1 for every game.
 	
 	
 	/** UC-2 :- As a Gambler make 1 bet so either win or loose 1. **/
@@ -16,11 +19,13 @@ public class Gambling_Simulator {
 		int random_Check = (int)Math.floor(Math.random()*2);
 		switch(random_Check) {
 			case 1 :
-				num_of_wins_in_day++;
+				num_of_wins_in_month++;  // counting number of wins for whole month.
+				num_of_wins_in_day++;     // counting number of wins for every day.
 				Account_balance += BET;   // increasing stake/account_balance by 1 if player won the bet.
 				break;
 			default :
-				num_of_losses_in_day++;
+				num_of_losses_in_month++;   // counting number of losses for whole month
+				num_of_losses_in_day++;     // counting number of wins for every day.
 				Account_balance -= BET;   // decreasing stake/account_balance by 1 if player loose the bet.
 		}	
 	}
@@ -28,8 +33,10 @@ public class Gambling_Simulator {
 	/** UC-3 :- As a Calculative Gambler if won or lost 50% of the stake, would resign for the day. **/
 	public static void gambling() {
 		int temp_balance = Account_balance;  // temporay variable to hold account balance 100.
+		
 		// calculating 50%
 		int fifty_percent = (50/100)*temp_balance;
+		
 		while(Account_balance <= (Account_balance + fifty_percent) ||
 			  Account_balance >= (Account_balance - fifty_percent)) {
 			
@@ -43,23 +50,27 @@ public class Gambling_Simulator {
 		System.out.println("Gambler total number of wins in a day :- " + num_of_wins_in_day);
 		System.out.println("Gambler total number of losses in a day :- " + num_of_losses_in_day + "\n");
 		
-		num_of_wins_in_day = 0;  // making zero bcoz for next day i want to count from fresh to print every day report.
-		num_of_losses_in_day = 0; // making zero bcoz for next day i want to count from fresh to print every day report
+		num_of_wins_in_day = 0;  // making zero bcoz for next day. i want to count from fresh to print every day report.
+		num_of_losses_in_day = 0; // making zero bcoz for next day. i want to count from fresh to print every day report
 		Account_balance = 100;  // makind 100 bcoz for next day player again starts from 100 only.
 	}
 	
-	/** UC-4 :- After 20 days of playing every day would like to know the total amount won or lost. **/
-	public static void twentyDays() {
-		for(int day = 1; day <= 20; day++) {     // gambling for 20 days
+	/** UC-5 :- Each month would like to know the days won and lost and by how much. **/
+	public static void gambledForMonth() {
+		for(int day = 1; day <= 30; day++) {     // In UC-5 , it mentioned for a month so changed UC-4=20 days to 30 days.
 			System.out.println("DAY-" + day + " :-");
 			gambling();
 		}
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Gambler total number of wins in a month :- " + num_of_wins_in_month);
+		System.out.println("Gambler total number of looses in a month :- " + num_of_losses_in_month);
+		System.out.println("-----------------------------------------------------");
 	}
 	
 	/** Main method(Program execution starts from main method.). **/
 	public static void main(String[] args) {
 		/** Displayed Welcome message **/
 		System.out.println("------------WELCOME TO GAMBLING SIMULATOR---------------\n");
-		twentyDays();
+		gambledForMonth();
 	}
 }
