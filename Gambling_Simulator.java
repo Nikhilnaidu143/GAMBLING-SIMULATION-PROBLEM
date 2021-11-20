d0be04518293b1921c0c21a05a0938aebcdd8770
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-20 07:15:13 +0530

    Start :- Displayed welcome message.

---
+++ Gambling_Simulator.java
@@ -0,0 +1,8 @@
+package com.gambling.simulator;
+
+public class Gambling_Simulator {
+	public static void main(String[] args) {
+		/** Displayed Welcome message **/
+		System.out.println("------------WELCOME TO GAMBLING SIMULATOR---------------\n");
+	}
+}
