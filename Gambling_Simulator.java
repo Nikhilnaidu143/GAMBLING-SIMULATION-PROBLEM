19a16c21b519a1440fba3f5e1d0d91503a9b8be1
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-20 07:23:21 +0530

    UC-1 :- UC-1 is solved with player stake and bet variables.

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -1,6 +1,12 @@
 package com.gambling.simulator;
 
 public class Gambling_Simulator {
+	/** Instance variables **/
+	static int Account_balance = 100;   // player start betting daily with a stake of 100.
+	
+	//constant variable
+	static final int BET = 1;  // player bets 1 for every game
+	
 	public static void main(String[] args) {
 		/** Displayed Welcome message **/
 		System.out.println("------------WELCOME TO GAMBLING SIMULATOR---------------\n");
