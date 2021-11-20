302080fa53b58f84044ec3ea48fc356a2acbe84c
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-20 09:43:08 +0530

    UC-4 :- Gambled for 20 days and printed daily report.

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -3,6 +3,8 @@ package com.gambling.simulator;
 public class Gambling_Simulator {
 	/** Instance variables **/
 	static int Account_balance = 100;   // player start betting daily with a stake of 100.
+	static int num_of_wins_in_day = 0;
+	static int num_of_losses_in_day = 0;
 	
 	//constant variable
 	static final int BET = 1;  // player bets 1 for every game
@@ -14,14 +16,12 @@ public class Gambling_Simulator {
 		int random_Check = (int)Math.floor(Math.random()*2);
 		switch(random_Check) {
 			case 1 :
+				num_of_wins_in_day++;
 				Account_balance += BET;   // increasing stake/account_balance by 1 if player won the bet.
-				System.out.println("Player won the bet.\n");
-				System.out.println("Player Account balance :- " + Account_balance);
 				break;
 			default :
+				num_of_losses_in_day++;
 				Account_balance -= BET;   // decreasing stake/account_balance by 1 if player loose the bet.
-				System.out.println("Player loose the bet.\n");
-				System.out.println("Player Account balance :- " + Account_balance);
 		}	
 	}
 	
@@ -30,23 +30,36 @@ public class Gambling_Simulator {
 		int temp_balance = Account_balance;  // temporay variable to hold account balance 100.
 		// calculating 50%
 		int fifty_percent = (50/100)*temp_balance;
-		
 		while(Account_balance <= (Account_balance + fifty_percent) ||
 			  Account_balance >= (Account_balance - fifty_percent)) {
 			
 			playerWinOrLoose();  //calling playerWinOrLoose method.
 			
-			if(Account_balance == 150 || Account_balance == 50) {
+			if(Account_balance == 150 || Account_balance == 50) {  // if these conditions satisfied then loop will break.
 				break;
 			}
 		}
+		System.out.println("Gambler Account balance :- " + Account_balance);
+		System.out.println("Gambler total number of wins in a day :- " + num_of_wins_in_day);
+		System.out.println("Gambler total number of losses in a day :- " + num_of_losses_in_day + "\n");
+		
+		num_of_wins_in_day = 0;  // making zero bcoz for next day i want to count from fresh to print every day report.
+		num_of_losses_in_day = 0; // making zero bcoz for next day i want to count from fresh to print every day report
+		Account_balance = 100;  // makind 100 bcoz for next day player again starts from 100 only.
 	}
 	
+	/** UC-4 :- After 20 days of playing every day would like to know the total amount won or lost. **/
+	public static void twentyDays() {
+		for(int day = 1; day <= 20; day++) {     // gambling for 20 days
+			System.out.println("DAY-" + day + " :-");
+			gambling();
+		}
+	}
 	
 	/** Main method(Program execution starts from main method.). **/
 	public static void main(String[] args) {
 		/** Displayed Welcome message **/
 		System.out.println("------------WELCOME TO GAMBLING SIMULATOR---------------\n");
-		gambling();
+		twentyDays();
 	}
 }
