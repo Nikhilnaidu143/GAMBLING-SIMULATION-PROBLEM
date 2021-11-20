6f18b866274ab57967a4e9c2b20da0d6533da7e9
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-20 07:39:19 +0530

    UC-2 :- Gambler gain 1$ when won and get loss 1$ when loose.

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -7,8 +7,28 @@ public class Gambling_Simulator {
 	//constant variable
 	static final int BET = 1;  // player bets 1 for every game
 	
+	
+	/** UC-2 :- As a Gambler make 1 bet so either win or loose 1. **/
+	public static void playerWinOrLoose() {
+		/* Random check for win or loose checking */
+		int random_Check = (int)Math.floor(Math.random()*2);
+		switch(random_Check) {
+			case 1 :
+				Account_balance += BET;   // increasing stake/account_balance by 1 if player won the bet.
+				System.out.println("Player won the bet.\n");
+				System.out.println("Player Account balance :- " + Account_balance);
+				break;
+			default :
+				Account_balance -= BET;   // decreasing stake/account_balance by 1 if player loose the bet.
+				System.out.println("Player loose the bet.\n");
+				System.out.println("Player Account balance :- " + Account_balance);
+		}	
+	}
+	
+	/** Main method(Program execution starts from main method.). **/
 	public static void main(String[] args) {
 		/** Displayed Welcome message **/
 		System.out.println("------------WELCOME TO GAMBLING SIMULATOR---------------\n");
+		playerWinOrLoose();
 	}
 }
